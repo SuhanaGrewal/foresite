40c43ea55a04b214a0a109e6133a2a716f36eb1f
SuhanaGrewal <suhanagrewal0407@gmail.com>
2026-08-13 21:53:38 +0530

    computed 150 batch tasks from the FRAMES questionnaire.

--- run_frames_batch.py
+++ run_frames_batch.py
@@ -21,7 +21,7 @@ from frames_data import build_corpus, load_frames_questions
 from orchestrator import orchestrate
 from trace_agent import clear_search_corpus, set_search_corpus
 
-N_QUESTIONS = 8
+N_QUESTIONS = 150
 ERROR_LOG_PATH = "frames_batch_errors.log"
 
 
